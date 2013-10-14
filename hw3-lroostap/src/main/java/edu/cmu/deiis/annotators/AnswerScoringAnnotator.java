51a0ce853cfb4fc0d674c4f9e00fdf36235afa4b
laleh roosta pour <roostapour@gmail.com>
2013-10-14 17:59:51 -0400

    task2.3 is done!

--- hw3-lroostap/src/main/java/edu/cmu/deiis/annotators/AnswerScoringAnnotator.java
+++ hw3-lroostap/src/main/java/edu/cmu/deiis/annotators/AnswerScoringAnnotator.java
@@ -1,17 +1,18 @@
 package edu.cmu.deiis.annotators;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
-import java.util.Collections;
-import java.util.Comparator;
 
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.FSIterator;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.cas.FSIndex;
+import org.cleartk.ne.type.NamedEntity;
+import org.cleartk.ne.type.NamedEntityMention; 
 
 import edu.cmu.deiis.types.*;
 
@@ -53,9 +54,51 @@ public class AnswerScoringAnnotator extends JCasAnnotator_ImplBase {
     Set<Annotation> annotationKeyList = nGramHashMap.keySet();
     annotationKeyList.remove(question);
     
+
+    Iterator<Annotation> qIter = aJCas.getAnnotationIndex(Question.type).iterator();
+    int questionBegin = 0;
+    int questionEnd = 0;
+    while(qIter.hasNext()){
+      Question q = (Question)qIter.next();
+      questionBegin = 0;
+      questionEnd = q.getEnd();
+    }
+    
+
+    // Finding the name entities in questions
+    int questionNE = 0;
+    FSIndex ne_index = aJCas.getAnnotationIndex(NamedEntityMention.type);
+    //System.out.println("NamedEntity ----> ");
+    //System.out.println(ne_index);
+    Iterator neIt = ne_index.iterator();
+    HashMap<String,Integer> mapENType = new HashMap<String,Integer>();
+    //System.out.println("hasNext");
+    //System.out.println(neIt.hasNext());
+    while(neIt.hasNext()){
+      NamedEntityMention nEM = (NamedEntityMention)neIt.next();
+      //System.out.println("I am here");
+      if(nEM.getBegin() >= questionBegin && nEM.getEnd() <= questionEnd){ 
+        if(nEM.getMentionType()!=null){
+          mapENType.put(nEM.getMentionType(), 0);
+          questionNE++;
+        }
+      }
+      if(nEM.getBegin() > questionEnd)
+        break;
+    }
+    
+    //System.out.println("question:");
+    //System.out.println(questionNE);
+    
+    ArrayList<AnswerScore> answerScoreList = new ArrayList<AnswerScore>();
+    ArrayList<Double> ngramScoreList = new ArrayList<Double>();
+    ArrayList<Integer> answerNE = new ArrayList<Integer>(); 
+    int answerMaxNE = 0; 
     /**
      * A simple NGram matching technique between a question and an answer
      */
+    //System.out.println("siiiiiiize");
+    //System.out.println(annotationKeyList.size());
     for (Annotation answer : annotationKeyList) {
       float numberOfMatches = (float) 0;
       float totalNumber = (float) 0;
@@ -69,17 +112,56 @@ public class AnswerScoringAnnotator extends JCasAnnotator_ImplBase {
         }
       }
       
+    //Checking for the named entities in answers to match with the question
+      neIt  = ne_index.iterator(); 
+      int answerNEType = 0;
+      while(neIt.hasNext()){
+        NamedEntityMention nEM = (NamedEntityMention)neIt.next();
+        if(nEM.getBegin()>=answer.getBegin() && nEM.getEnd()<= answer.getEnd()){
+          if(nEM.getMentionType()!=null){
+            if(mapENType.get(nEM.getMentionType())!=null){
+              answerNEType++;
+            }
+          }
+        }
+        if(nEM.getBegin() > answer.getEnd())
+          break;
+      }
+      answerNE.add(answerNEType);
+      if(answerNEType > answerMaxNE)
+        answerMaxNE = answerNEType;
+      
+      //System.out.println(answerNEType);
+      //System.out.println("and max");
+      //System.out.println(answerMaxNE);
       /**
        * Creating an AnswerScore and set the variables
        */
       AnswerScore ansScore = new AnswerScore(aJCas);
       ansScore.setCasProcessorId(this.getClass().getName());
+      //System.out.println(answer.getType());
+      //System.out.println(answer.getCoveredText());
       ansScore.setAnswer((Answer) answer);
       ansScore.setBegin(answer.getBegin());
       ansScore.setEnd(answer.getEnd());
-      ansScore.addToIndexes();
       ansScore.setConfidence(1);
-      ansScore.setScore((float) numberOfMatches / totalNumber);
+      //ansScore.setScore((float) numberOfMatches / totalNumber);
+      answerScoreList.add(ansScore);
+      ngramScoreList.add((double) numberOfMatches / totalNumber);
+    
+    }
+    
+    
+    for(int answer=0;answer<answerScoreList.size();answer++){
+      AnswerScore ansSc = answerScoreList.get(answer);
+      double neScore = 0;
+      double combinedScore = 0;
+      double ngramScore = ngramScoreList.get(answer);
+      neScore = (double)answerNE.get(answer)/answerMaxNE;
+      //System.out.println(answerNE.get(answer));
+      combinedScore = (ngramScore + neScore)/2;
+      ansSc.setScore(ngramScore);
+      ansSc.addToIndexes();
     }
      
   }
